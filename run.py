280064a73aebf2a995b6aeb53d259005b93e6d20
dima-yurchuk <dimayurchuk20@gmail.com>
2021-10-23 19:16:21 +0300

    Innitial commit

---
+++ run.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return 'Hello world'
+
+if __name__=='__main__':
+    app.run(debug=True)
