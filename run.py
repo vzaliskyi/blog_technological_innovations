65575d43b279486a01363b69665bea415f39f39e
dima-yurchuk <dimayurchuk20@gmail.com>
2021-10-29 17:36:14 +0300

    Create basic structure

--- run.py
+++ run.py
@@ -1,10 +1,4 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    return 'Hello world'
+from app import app
 
 if __name__=='__main__':
     app.run(debug=True)
