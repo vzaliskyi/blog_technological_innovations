086222519931ec02a17539fc75ef5aa406d9b34f
dima-yurchuk <dimayurchuk20@gmail.com>
2021-11-04 20:00:51 +0200

    change gitignore

--- app/user/models.py
+++ app/user/models.py
@@ -13,8 +13,8 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(30), nullable=False)
     picture = db.Column(db.String(20), nullable=False,
                         server_default='default.jpg')
 
