23b273fb5fd03d179386bb91c4aee888729c0d1e
dima-yurchuk <dimayurchuk20@gmail.com>
2021-11-11 10:24:47 +0200

    Fixing linter

--- app/__init__.py
+++ app/__init__.py
@@ -16,5 +16,5 @@ from .user import user_bp  # linting: disable=E402
 app.register_blueprint(blog_bp, url_prefix='')
 app.register_blueprint(user_bp, url_prefix='/auth')
 
-from app import views  # linting: disable=unused-import
-from app import forms  # linting: disable=unused-import
+from app import views
+from app import forms
