4bce005c1f68ea62102a9d64c79a7dc666c59bf0
dima-yurchuk <dimayurchuk20@gmail.com>
2021-11-22 11:27:27 +0200

    fixing linter

--- manage.py
+++ manage.py
@@ -1,8 +1,6 @@
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 # from flask_script import Command, prompt_bool
-from app.blog.models import *
-from app.user.models import *
 from app import db, app
 
 migrate = Migrate(app, db)
