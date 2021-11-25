48001e9cb4cf5a4ddde7068bf391f51d51ce7f92
dima-yurchuk <dimayurchuk20@gmail.com>
2021-11-25 13:33:45 +0200

    tests for comments and likes

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ from flask_testing import TestCase
 from app import db, app
 
 # from flask import url_for
-from app.user.models import User, Post, Category
+from app.user.models import User
+from app.blog.models import *
 
 
 class BaseTestCase(TestCase):
@@ -11,6 +12,20 @@ class BaseTestCase(TestCase):
                       SECRET_KEY='asfdsfsaaffdf', WTF_CSRF_ENABLED=False)
     db.drop_all()
     db.create_all()
+    db.session.add_all([Category(name='Смартфони'),
+                        Post(category_id=1, user_id=1, title='Test title',
+                             content='Test content for test title'),
+                        User(username='unit_tester_main',
+                             email='unit_tester_main@gmail.com',
+                             password='qwerTy#45'),
+                        User(username='unit_tester_comment',
+                             email='unit_tester_comment@gmail.com',
+                             password='qwerTy#45'),
+                        Post(category_id=1, user_id=2, title='Post for test '
+                                                             'likes',
+                             content='Test content(post for test likes)'),
+                        ])
+    db.session.commit()
 
     def create_app(self):
         return app
@@ -101,8 +116,9 @@ class BaseTestCase(TestCase):
                 '<legend class="border-bottom mb-4">Вхід</legend>' in
                 response.get_data(as_text=True))
             response = self.client.post('/auth/login',
-                                        data={'email': 'team3member@gmail.com',
-                                              'password': '12345qaZ!'},
+                                        data={'email': 'unit_tester_main'
+                                                       '@gmail.com',
+                                              'password': 'qwerTy#45'},
                                         follow_redirects=True)
             self.assert200(response)
             # redirect_url = url_for('/auth/login', next='/auth/account')
@@ -114,7 +130,8 @@ class BaseTestCase(TestCase):
                 'про мене</legend>' in response.get_data(as_text=True)
             )
             self.assertTrue(
-                '<em class="card-title text-muted">team3member@gmail.com</em>'
+                '<em class="card-title text-muted">'
+                'unit_tester_main@gmail.com</em>'
                 in response.get_data(as_text=True)
             )
             # користувач тепер має доступ до сторінки аккаунту
@@ -158,8 +175,9 @@ class BaseTestCase(TestCase):
 
             # якщо користувач вводить невірний пароль
             response = self.client.post('/auth/login',
-                                        data={'email': 'team3member@gmail.com',
-                                              'password': '12345q!'},
+                                        data={'email': 'unit_tester_main'
+                                                       '@gmail.com',
+                                              'password': 'qwerty345'},
                                         follow_redirects=True)
             self.assert200(response)
             self.assertMessageFlashed('Введено невірний пароль.',
@@ -168,8 +186,8 @@ class BaseTestCase(TestCase):
 
     def test6_create_post(self):
         with self.client:
-            db.session.add(Category(name='Смартфони'))
-            db.session.commit()
+            # db.session.add(Category(name='Смартфони'))
+            # db.session.commit()
 
             response = self.client.post('/auth/login',
                                         data={'email': 'team3member@gmail.com',
@@ -196,6 +214,138 @@ class BaseTestCase(TestCase):
             self.assertTrue('<h3 class="text-center mb-0">Post_tester22</h3>'
                             in response.get_data(as_text=True)
                             )
+
+    # перевірка для нeавторизованого користувача
+    def test7_access_to_comment_for_unauth_user(self):
+        comment1 = Comment(user_id=1, post_id=1, text='Test comment')
+        db.session.add(comment1)
+        db.session.commit()
+        response = self.client.get('/post/1',
+                                   content_type='html/text')
+        self.assert200(response)
+        self.assertFalse(
+            'Коментувати'
+            in response.get_data(as_text=True)
+        )
+        self.assertFalse(
+            'Надіслати'
+            in response.get_data(as_text=True)
+        )
+        self.assertTrue(
+            'Авторизуйтесь, щоб мати можливість залишати коментарі.'
+            in response.get_data(as_text=True)
+        )
+        # якщо користувач НЕ авторизований, він НЕ може видалити коментарі
+        response = self.client.post('/comment/1/delete',
+                                    follow_redirects=True)
+        self.assert403(response)
+        self.assertTrue(
+            'Ви не маєте прав на видалення даного коментаря'
+            in response.get_data(as_text=True)
+        )
+        # якщо користувач НЕ авторизований, він НЕ може додавати коментарі
+        response = self.client.post('/post/1',
+                                    data={'comment': 'Great comment!'},
+                                    follow_redirects=True)
+        self.assertTrue(
+            'Авторизуйтесь, щоб мати можливість залишати коментарі.'
+            in response.get_data(as_text=True)
+        )
+
+    # перевірка для авторизованого користувача
+    def test8_access_to_comment_for_auth_user(self):
+        response = self.client.post('/auth/login',
+                                    data={
+                                        'email': 'unit_tester_main@gmail.com',
+                                        'password': 'qwerTy#45'},
+                                    follow_redirects=True)
+        self.assert200(response)
+        self.assertMessageFlashed('Користувач успішно увійшов у свій '
+                                  'аккаунт!', category='success')
+        # якщо користувач авторизований, він може додавати коментарі
+        response = self.client.post('/post/1',
+                                    data={'comment': 'Great!'},
+                                    follow_redirects=True)
+        self.assertTrue(
+            'Коментарі'
+            in response.get_data(as_text=True)
+        )
+        self.assertTrue(
+            'Надіслати'
+            in response.get_data(as_text=True)
+        )
+        self.assertFalse(
+            'Авторизуйтесь, для того, щоб мати можливість писати коментарі'
+            in response.get_data(as_text=True)
+        )
+        self.assertTrue(
+            'Great!'
+            in response.get_data(as_text=True)
+        )
+        # якщо користувач авторизований, він може видалити коментарі
+        self.assertTrue(
+            'title="Видалити"'
+            in response.get_data(as_text=True)
+        )
+        response = self.client.post('/comment/2/delete',
+                                    follow_redirects=True)
+        self.assert200(response)
+        self.assertFalse(
+            'Great!'
+            in response.get_data(as_text=True)
+        )
+
+    # перевірка для нeавторизованого користувача
+    def test9_access_to_like_for_unauth_user(self):
+        response = self.client.get('/post/1',
+                                   content_type='html/text')
+        # print(response.get_data(as_text=True))
+        self.assert200(response)
+        response = self.client.get('/post/1/like',
+                                   content_type='html/text')
+        self.assert401(response)
+        self.assertTrue(
+            'The server could not verify that you are authorized to access '
+            'the URL requested '
+            in response.get_data(as_text=True)
+        )
+
+    # перевірка для нeавторизованого користувача
+    def test10_access_to_like_for_auth_user(self):
+        response = self.client.post('/auth/login',
+                                    data={'email': 'unit_tester_main@gmail.'
+                                                   'com',
+                                          'password': 'qwerTy#45'},
+                                    follow_redirects=True)
+        self.assert200(response)
+        self.assertMessageFlashed('Користувач успішно увійшов у свій '
+                                  'аккаунт!', category='success')
+        response = self.client.get('/post/1',
+                                   content_type='html/text')
+        self.assert200(response)
+        # перевірка того, що користувач НЕ може оцінювати власні пости
+        response = self.client.get('/post/1/like',
+                                   content_type='html/text')
+        self.assert403(response)
+        self.assertTrue(
+            'Ви не можете оцінювати власні публікації'
+            in response.get_data(as_text=True)
+        )
+        # перевірка того, що користувач може оцінювати чужі пости
+        response = self.client.get('/post/2/like',
+                                   content_type='html/text')
+        self.assertTrue(
+            'You should be redirected automatically to target URL:'
+            in response.get_data(as_text=True)
+        )
+        response = self.client.get('/post/2',
+                                   content_type='html/text')
+        # print(response.get_data(as_text=True))
+        self.assert200(response)
+        self.assertTrue(
+            'class="bi" data-toggle="tooltip">1</a>'
+            in response.get_data(as_text=True)
+        )
 
 
 if __name__ == '__main__':
