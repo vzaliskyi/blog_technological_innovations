881062bf9725e053113e244a549e7b8067a6bbd0
fantastic-member <78656799+fantastic-member@users.noreply.github.com>
2021-12-07 13:09:23 +0200

    added pagination for home page

--- app/views.py
+++ app/views.py
@@ -6,23 +6,11 @@ from flask import render_template, redirect, url_for, request, flash, \
 
 @app.route('/')
 def home():
-    # print("all_posts")
+    page = request.args.get('page', 1, type=int)
+    print("page=", page)
+
     posts = Post.query.order_by(Post.created_at.desc())
-    # posts = db.session.query(Post).join(Like).filter(Like.post_id==Post.id,
-    # Like.status == True).filter(Like.user_id == 3).all()
-    # posts = Post.query.order_by(Post.user_id)
-    # print('кількість лайків', posts.first().total_likes())
-    # posts = Post.query.all()
+
+    posts = posts.paginate(page=page, per_page=5)
+
     return render_template('home.html', title='TechBlog', posts=posts)
-
-
-# сюди передавати мабуть ще повідомлення про помилку @app.errorhandler(401)
-# @app.errorhandler(401)
-# def unauthorized(e):
-#     if 'like' in request.path or 'dislike' in request.path:
-#         flash('Авторизуйтеся або зареєструйтеся, щоб мати можливість '
-#               'оцінювати публікації', 'info')
-#     else:
-#         flash('Для доступу до цієї сторінки необхідно спершу авторизуватись',
-#               'info')
-#     return redirect(url_for('user_bp_in.login'), code=401)
