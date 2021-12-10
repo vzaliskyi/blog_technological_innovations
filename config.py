d76c9a8e37db71e6d6a0ce5f738873729b21452b
dima-yurchuk <dimayurchuk20@gmail.com>
2021-12-10 16:08:30 +0200

    deploy in heroku

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ WTF_CRSF_ENAVLED = True
 # Database
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                           'sqlite:///' + os.path.join(basedir, 'blog.db')
+print('sqlite:///' + os.path.join(basedir, 'blog.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 MAIL_SERVER = 'smtp.googlemail.com'
